91003e9a368078278144ff83afdc3c0224f738c2
Raki Rahman <mdrrahman@microsoft.com>
2022-04-20 07:40:21 -0400

    Removing placeholder stuff

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,8 +1,3 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
 package com.mycompany.app;
 
 public class App 
@@ -10,5 +5,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Remote World!" );
+        System.out.println( "Hello Remote World 2!" );
     }
 }
