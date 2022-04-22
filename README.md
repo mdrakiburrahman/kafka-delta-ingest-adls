425b741af4cf613c4e1e56f331c9ec1c6dfa6dac
Raki Rahman <mdrakiburrahman@gmail.com>
2022-04-21 21:49:03 -0400

    Spark and local read working, jar is not

--- README.md
+++ README.md
@@ -22,6 +22,63 @@ mvn clean package
 java -jar target/my-app-1.0-SNAPSHOT.jar
 ```
 
+## Run Spark locally
+
+```bash
+/opt/spark/sbin/start-master.sh # http://localhost:8080/ Master UI
+/opt/spark/sbin/start-worker.sh spark://$(hostname):7077 # http://localhost:8081/ Workers UI
+
+# Go into Scala Shell with Delta - http://localhost:4040/ Jobs UI
+/opt/spark/bin/spark-shell --packages io.delta:delta-core_2.12:1.0.0 \
+  --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
+  --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
+
+# [OPTIONAL] Go into PySpark Shell
+# /opt/spark/bin/pyspark
+```
+
+Create a bunch of Delta files:
+
+```scala
+val table = "/tmp/delta_standalone_test/"
+for (i <- 0 to 2) { // 3 Delta Versions - Scala loops are inclusive of bound
+   spark.range(i * 10, (i + 1) * 10)
+      .map(x => (x, x % 5, s"foo-${x % 2}"))
+      .toDF("c1", "c2", "c3")
+      .write
+      .mode("append")
+      .format("delta")
+      .save(table)
+}
+
+spark.read.format("delta").load(table).count
+// res1: Long = 30
+
+spark.read.format("delta").load(table).limit(5).show
+// +---+---+-----+
+// | c1| c2|   c3|
+// +---+---+-----+
+// | 56|  1|foo-0|
+// | 57|  2|foo-1|
+// | 58|  3|foo-0|
+// | 59|  4|foo-1|
+// | 60|  0|foo-0|
+// +---+---+-----+
+```
+
+And we see:
+```bash
+ls /tmp/delta_standalone_test
+# _delta_log
+# part-00000-195768ae-bad8-4c53-b0c2-e900e0f3eaee-c000.snappy.parquet
+# part-00000-53c3c553-f74b-4384-b9b5-7aa45bc2291b-c000.snappy.parquet
+# ..=
+
+ls /tmp/delta_standalone_test/_delta_log/*.json
+# /tmp/delta_standalone_test/_delta_log/00000000000000000000.json  /tmp/delta_standalone_test/_delta_log/00000000000000000002.json
+# /tmp/delta_standalone_test/_delta_log/00000000000000000001.json
+```
+
 ## Debug locally
 
 1. **Edit:**
--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,50 @@
 package com.mycompany.app;
 
+// https://delta-io.github.io/connectors/latest/delta-standalone/api/java/io/delta/standalone/Snapshot.html
+import io.delta.standalone.DeltaLog;
+import io.delta.standalone.Snapshot;
+import io.delta.standalone.data.CloseableIterator;
+import io.delta.standalone.data.RowRecord;
+
+import org.apache.hadoop.conf.Configuration;
+
 public class App 
 {
+    public static void printSnapshotDetails(String title, Snapshot snapshot) {
+        System.out.println("===== " + title + " =====");
+        System.out.println("version: " + snapshot.getVersion());
+        System.out.println("number data files: " + snapshot.getAllFiles().size());
+        System.out.println("data files:");
+        snapshot.getAllFiles().forEach(file -> System.out.println(file.getPath()));
+
+        CloseableIterator<RowRecord> iter = snapshot.open();
+
+        System.out.println("\ndata rows:");
+        RowRecord row = null;
+        int numRows = 0;
+        while (iter.hasNext()) {
+            row = iter.next();
+            numRows++;
+
+            Long c1 = row.isNullAt("c1") ? null : row.getLong("c1");
+            Long c2 = row.isNullAt("c2") ? null : row.getLong("c2");
+            String c3 = row.getString("c3");
+            System.out.println(c1 + " " + c2 + " " + c3);
+        }
+        System.out.println("\nnumber rows: " + numRows);
+        System.out.println("data schema:");
+        System.out.println(row.getSchema().getTreeString());
+        System.out.println("\n");
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello Remote World!" );
-        System.out.println( "Hello Remote World 2!" );
+        System.out.println( "Reading pre-created Delta Files from: /tmp/delta_standalone_test" );
+
+        DeltaLog log = DeltaLog.forTable(new Configuration(), "/tmp/delta_standalone_test/");
+
+        printSnapshotDetails("current snapshot", log.snapshot());
+        printSnapshotDetails("version 0 snapshot", log.getSnapshotForVersionAsOf(0));
+        printSnapshotDetails("version 1 snapshot", log.getSnapshotForVersionAsOf(1));
+        printSnapshotDetails("version 2 snapshot", log.getSnapshotForVersionAsOf(2));
     }
 }
